0f9c0aea8ddfbb6287aa7abb49f087599dd08317
hardalgorithm <gritsenko.dhr@gmail.com>
2025-07-30 16:19:24 +0300

    fix: background Shake services now start automatically after reboot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.paranoid.privacylock"
         minSdk = 28
         targetSdk = 35
-        versionCode = 7
-        versionName = "v1.1.5"
+        versionCode = 8
+        versionName = "v1.1.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
--- app/src/main/java/com/paranoid/privacylock/receivers/BootReceiver.kt
+++ app/src/main/java/com/paranoid/privacylock/receivers/BootReceiver.kt
@@ -6,20 +6,27 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.paranoid.privacylock.services.LockService
+import com.paranoid.privacylock.services.ShakeService
 import com.paranoid.privacylock.storage.ParanoidSettingStorage
 import com.paranoid.privacylock.util.isDeviceAdminActive
 
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_USER_UNLOCKED) {
             if (isDeviceAdminActive(context)) {
                 val settingStorage = ParanoidSettingStorage(context)
                 if (settingStorage.enabledMinutes>0){
-                    val serviceIntent = Intent(context, LockService::class.java)
-                    serviceIntent.putExtras(Bundle().apply {
+                    val lockServiceIntent = Intent(context, LockService::class.java)
+                    lockServiceIntent.putExtras(Bundle().apply {
                         putInt("minutesDelay", settingStorage.enabledMinutes)
                     })
-                    ContextCompat.startForegroundService(context, serviceIntent)
+                    ContextCompat.startForegroundService(context, lockServiceIntent)
+                }
+                if (settingStorage.isActiveShake){
+                    val shakeServiceIntent = Intent(context, ShakeService::class.java)
+                    ContextCompat.startForegroundService(context, shakeServiceIntent)
                 }
             }
         }
